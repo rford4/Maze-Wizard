fdac679d78578aee9a2d6ca6632f9c67ac35967b
Ryan Ford <ryan@rfordconsulting.com>
2025-11-23 15:17:14 -0800

    Updated assembly name of main project.

--- MazeWizard.Presentation.Tests/Fixtures/IntegrationTest.cs
+++ MazeWizard.Presentation.Tests/Fixtures/IntegrationTest.cs
@@ -14,7 +14,7 @@ public class IntegrationTest
             "bin",
             "Debug",
             "net10.0-windows",
-            "mazewizard.presentation.exe"));
+            "mazewizard.exe"));
 
         ProcessStartInfo processStartInfo = new()
         {
